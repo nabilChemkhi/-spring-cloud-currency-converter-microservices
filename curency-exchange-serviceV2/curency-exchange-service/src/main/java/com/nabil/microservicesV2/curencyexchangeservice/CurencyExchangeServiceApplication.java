c390e1a9dc3d0c0ceebee424be9581588a248b11
nabilChemkhi <nabil.chemkhi@esprit.tn>
2022-01-11 09:30:18 +0100

    rabitmq and zipkin config

--- curency-exchange-serviceV2/curency-exchange-service/src/main/java/com/nabil/microservicesV2/curencyexchangeservice/CurencyExchangeServiceApplication.java
+++ curency-exchange-serviceV2/curency-exchange-service/src/main/java/com/nabil/microservicesV2/curencyexchangeservice/CurencyExchangeServiceApplication.java
@@ -1,8 +1,12 @@
 package com.nabil.microservicesV2.curencyexchangeservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +16,12 @@ public class CurencyExchangeServiceApplication {
 		SpringApplication.run(CurencyExchangeServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start (CurrencyExchangeRepository crExRep){
+		return  args -> {
+			crExRep.save(new CurrencyExchange(10001L,"USD","TND", BigDecimal.valueOf(3)));
+			crExRep.save(new CurrencyExchange(10002L,"EUR","TND", BigDecimal.valueOf(3.5)));
+
+		};
+	}
 }
